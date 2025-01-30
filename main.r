1c9ece4ab8389b8d86532cc245ebc301aa4916cb
Mahi <md.mahatabmahimn@gmail.com>
2025-01-31 01:01:54 +1300

    tested some R code

--- main.r
+++ main.r
@@ -1,17 +1,13 @@
 # Create sample data
-
 fruits <- c("Apple", "Orange", "Banana", "Grape")
 
 quantity <- c(10, 5, 8, 12)
 
 
-
 # Create a data frame
-
 fruit_data <- data.frame(Fruit = fruits, Quantity = quantity)
 
 
 
 # Plot a bar chart
-
 barplot(Quantity ~ Fruit, data = fruit_data, xlab = "Fruit", ylab = "Quantity", main = "Fruit Quantity")
