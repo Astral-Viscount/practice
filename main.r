3e17c4104c7ae7da6cf46a94fc0f024e2899f4bc
Mahi <md.mahatabmahimn@gmail.com>
2025-01-31 00:53:38 +1300

    made a R file

---
+++ main.r
@@ -0,0 +1,17 @@
+# Create sample data
+
+fruits <- c("Apple", "Orange", "Banana", "Grape")
+
+quantity <- c(10, 5, 8, 12)
+
+
+
+# Create a data frame
+
+fruit_data <- data.frame(Fruit = fruits, Quantity = quantity)
+
+
+
+# Plot a bar chart
+
+barplot(Quantity ~ Fruit, data = fruit_data, xlab = "Fruit", ylab = "Quantity", main = "Fruit Quantity")
