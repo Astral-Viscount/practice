1c3378cbf038517d9d3e28c1d03832d5f69418e9
Mahi <md.mahatabmahimn@gmail.com>
2025-03-31 20:00:05 +1300

    practicing c

--- cs.c
+++ cs.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
-int main(void) {
-    printf("Hello, World!");
+int main(void)
+{
+    int num;
+    printf("Number: ");
+    scanf("%i", &num);
+
+    int fact = 1;
+
+    for(int i = 1; i < num + 1; i++)
+    {
+        fact *= i;
+    }
+
+    printf("%i! = %i", num, fact);
 }
--- main.c
+++ main.c
@@ -1,6 +1,49 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {	
-    printf("Hello, World!");
+//Functions
+int guessing_game(void);
+
+int main()
+{
+
     return 0;
+}
+
+//Guessing game
+int guessing_game(void)
+{
+    // Generate a random number using the rand() function
+    int upp = 100;
+    int low = 1;
+    int num = rand() % (upp - low + 1) + low;
+
+    int guess = 0;
+
+    printf("   Welcome to the number guessing game!\n");
+    printf("<<------------------------------------->>\n\n");
+
+    while (1 == 1)
+    {
+        int user;
+        printf("Guess the number: ");
+        scanf("%i", &user);
+
+        if (user > num)
+        {
+            printf("The number is smaller!\nTry again!");
+            guess++;
+        }
+        else if (user < num)
+        {
+            printf("The number is bigger!\nTry again!");
+            guess++; 
+        }
+        else
+        {
+            printf("\nCorrect!!! ");
+            printf("It took you %i guesses to get the correct number!\n", guess);
+            break;
+        }       
+    }
 }
